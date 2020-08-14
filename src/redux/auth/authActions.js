f468c9e75a4371215d4f3112e4458be775ba675f
Tay CJ <cjtay@outlook.sg>
2020-08-14 17:58:05 +0800

    imac shorten arrow functions in actions

--- src/redux/auth/authActions.js
+++ src/redux/auth/authActions.js
@@ -5,28 +5,20 @@ import {
     LOGOUT_REQUEST,
 } from './authTypes';
 
-export const login = () => {
-    return {
-        type: LOGIN_REQUEST,
-    };
-};
+export const login = () => ({
+    type: LOGIN_REQUEST,
+});
 
-export const loginSuccess = (userProfile) => {
-    return {
-        type: LOGIN_SUCCESS,
-        payload: userProfile,
-    };
-};
+export const loginSuccess = (userProfile) => ({
+    type: LOGIN_SUCCESS,
+    payload: userProfile,
+});
 
-export const loginFailure = (error) => {
-    return {
-        type: LOGIN_FAILURE,
-        payload: error,
-    };
-};
+export const loginFailure = (error) => ({
+    type: LOGIN_FAILURE,
+    payload: error,
+});
 
-export const logout = () => {
-    return {
-        type: LOGOUT_REQUEST,
-    };
-};
+export const logout = () => ({
+    type: LOGOUT_REQUEST,
+});
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './auth/authReducer';
 
 const rootReducer = combineReducers({
-    auth: authReducer,
+    auth: authReducer, // example of authentication
+    // example of basic static value in string or numbers
+    // example of objects
+    // example of nested objects - deep copy and use immer
+    // example of arrays
+    // example of nested arrays - deep copy and use immer
+    // example of API calls / Async Await
 });
 
 export default rootReducer;
